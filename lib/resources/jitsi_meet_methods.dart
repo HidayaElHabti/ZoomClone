9e481555e8d9ade0d00b02431227b25c4b57a012
Hidaya El Habti <hadouya24@gmail.com>
2022-08-21 13:11:28 +0100

    Meetings History

--- lib/resources/jitsi_meet_methods.dart
+++ lib/resources/jitsi_meet_methods.dart
@@ -1,9 +1,11 @@
 import 'package:jitsi_meet/feature_flag/feature_flag.dart';
 import 'package:jitsi_meet/jitsi_meet.dart';
 import 'package:zoom_clone/resources/auth_methods.dart';
+import 'package:zoom_clone/resources/firestore_methods.dart';
 
 class JitsiMeetMethods {
   final AuthMethods _authMethods = AuthMethods();
+  final FirestoreMethods _firestoreMethods = FirestoreMethods();
   void createMeeting(
       {required String roomName,
       required bool isAudioMuted,
@@ -27,6 +29,7 @@ class JitsiMeetMethods {
         ..audioMuted = isAudioMuted
         ..videoMuted = isVideoMuted;
 
+      _firestoreMethods.addToMeetingHistory(roomName);
       await JitsiMeet.joinMeeting(options);
     } catch (error) {
       print("error: $error");
